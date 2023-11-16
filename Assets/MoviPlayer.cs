31381af1b60c1d960ccbd1ee91fa5a6f73871b03
Wagner-Filho <wagner.filho016@gmail.com>
2023-11-16 17:42:49 -0300

    Começo da putaria

--- Assets/MoviPlayer.cs
+++ Assets/MoviPlayer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class MoviPlayer : MonoBehaviour
@@ -8,7 +9,11 @@ public class MoviPlayer : MonoBehaviour
     [SerializeField] Rigidbody2D _rb;
     [SerializeField] float _moveX;
     [SerializeField] float _moveY;
-    
+    [SerializeField] float _jumpF;
+
+    [SerializeField] bool _checkGround;
+    [SerializeField] bool isFacingRight;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +28,44 @@ public class MoviPlayer : MonoBehaviour
         _rb.velocity = new Vector2(_moveX, _rb.velocity.y);
         //_rb.velocity = new Vector2(_moveX, _moveY);
 
-        if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Space) && _checkGround == true)
         {
-            _rb.AddForce(new Vector2(0, 200));
+            _rb.AddForce(new Vector2(0, _jumpF*10));
         }
+
+        if (_moveX > 0 && isFacingRight == false)
+        {
+            Flip();
+        }
+
+        else if (_moveX < 0 && isFacingRight == true)
+        {
+            Flip();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            _checkGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Ground")
+        {
+            _checkGround = false;
+        }
+    }
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+        // Flip collider over the x-axis
+
     }
 }
